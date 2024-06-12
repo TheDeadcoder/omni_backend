8a0124559e8744b1ed504a84c843ee14227df942
Saadmrp1038 <saadmrp222@gmail.com>
2024-06-12 23:28:56 +0600

    alembic version folder added

--- create_tables.py
+++ create_tables.py
@@ -1,5 +1,5 @@
 from database import Base, engine
-from models import PrimaryUser, SecondaryUser, HistoryOfIllness, Hospital, CalendarEntry, CalendarEntryDetails
+# from models import PrimaryUser, SecondaryUser, HistoryOfIllness, Hospital, CalendarEntry, CalendarEntryDetails
 
 # Create all tables
 Base.metadata.create_all(bind=engine)
